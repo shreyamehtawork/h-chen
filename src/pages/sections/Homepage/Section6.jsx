7dd5e3281f6dfc7db85c6592fa75b18c13c73dcb
Shreya Mehta <workwithshreyamehta@gmail.com>
2025-08-06 00:56:44 +0530

    animations

--- src/pages/sections/Homepage/Section6.jsx
+++ src/pages/sections/Homepage/Section6.jsx
@@ -4,8 +4,8 @@ import herovideo from "../../../assets/hero.mp4";
 
 function Section6() {
   return (
-    <div className="hero-video-wrapper container text-center my-5">
-      <video className="hero-video" controls playsInline muted autoPlay loop>
+    <div className="hero-container">
+      <video className="bg-video" autoPlay muted loop playsInline>
         <source src={herovideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
