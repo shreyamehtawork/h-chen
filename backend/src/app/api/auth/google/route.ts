afc5357fca6f3170b966da159f78bb72fb9177a2
Dolamani Rohidas <dolamanirohidas696@gmail.com>
2025-08-20 01:20:14 +0530

    redirect url changed for google authentication

--- backend/src/app/api/auth/google/route.ts
+++ backend/src/app/api/auth/google/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
 
   const params = new URLSearchParams({
     client_id: process.env.GOOGLE_CLIENT_ID!,
-    redirect_uri: `${process.env.NEXTAUTH_URL || "http://localhost:3000"}/api/auth/google/callback`,
+    redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL!}/api/auth/google/callback`,
     response_type: "code",
     scope: "openid email profile",
     access_type: "offline",
