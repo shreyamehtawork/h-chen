7444ea8a6eaf2d78c2d1f0c82716a1602a8816b3
Shreya Mehta <workwithshreyamehta@gmail.com>
2025-08-30 00:46:08 +0530

    dashboard w/o user logged in

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -52,7 +52,7 @@ function Nav() {
           </button>
 
           <div
-            className="collapse navbar-collapse d-lg-flex"
+            className="collapse navbar-collapse d-lg-flex "
             id="navbarsExample11"
           >
             <ul className="navbar-nav ">
@@ -169,15 +169,28 @@ function Nav() {
                 </>
               ) : (
                 <>
-                  <a href="/login" style={{ textDecoration: "none" }}>
-                    Login
-                  </a>
-                  {/* <a
-                    href="/register"
-                    style={{ textDecoration: "none", marginRight: "10px" }}
+                  <div
+                    className="d-flex align-items-center"
+                    style={{ marginLeft: "5px", marginRight: "20px" }}
                   >
-                    Register
-                  </a> */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-person"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                    </svg>
+                    <Link
+                      to={"/login"}
+                      className="text-decoration-none text-dark"
+                      style={{ padding: " 0px 5px " }}
+                    >
+                      Login
+                    </Link>
+                  </div>
                 </>
               )}
             </div>
