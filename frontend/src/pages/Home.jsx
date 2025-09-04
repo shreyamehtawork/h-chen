45ef350a655d508a57938a9bbda21837e1a43734
Shreya Mehta <workwithshreyamehta@gmail.com>
2025-09-04 20:41:41 +0530

    Marquee removed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,56 +14,7 @@ function Home() {
       <Section1 />
 
       <Section2 />
-      <div
-        style={{
-          background: "black",
-          color: "white",
-          height: "50px",
-          overflow: "hidden",
-          display: "flex",
-          alignItems: "center",
-          position: "relative",
-          fontFamily: "Geo",
-          fontWeight: "bold",
-          padding: "35px",
-        }}
-      >
-        <div
-          style={{
-            display: "inline-block",
-            whiteSpace: "nowrap",
-            paddingLeft: "100%",
-            animation: "scroll-left 80s linear infinite",
-          }}
-        >
-          MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50%
-          OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL
-          PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥
-          &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE !
-          50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL
-          PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥
-          &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE !
-          50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL
-          PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥
-          &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE !
-          50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL
-          PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥
-          &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE !
-          50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL
-          PRODUCTS &nbsp;🔥 &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥
-          &nbsp; MEGA SALE ! 50% OFF ON ALL PRODUCTS &nbsp;🔥 &nbsp;{" "}
-        </div>
 
-        {/* Keyframes injected inline */}
-        <style>
-          {`
-          @keyframes scroll-left {
-            0% { transform: translateX(0); }
-            100% { transform: translateX(-100%); }
-          }
-        `}
-        </style>
-      </div>
       <Section3 />
       <ReviewSection />
       <Section5 />
